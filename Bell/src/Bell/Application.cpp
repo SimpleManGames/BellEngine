0800da1b275c199a5281b0fa841ffaa0e652a2b3
Riley <simplemangame@gmail.com>
2019-02-24 21:16:14 -0800

    Added a small test to application to test input polling

--- Bell/src/Bell/Application.cpp
+++ Bell/src/Bell/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Bell {
     Application* Application::s_Instance = nullptr;
 
@@ -47,6 +49,9 @@ namespace Bell {
             // Update each layer
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
+
+            auto[x, y] = Input::GetMousePosition();
+            B_CORE_TRACE("{0}, {1}", x, y);
 
             m_Window->OnUpdate();
         }

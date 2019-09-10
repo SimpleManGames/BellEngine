55ec6693a2947eed106c08ad361cec5c93afc861
Riley <simplemangame@gmail.com>
2019-09-09 21:04:48 -0700

    Added minor uniform support for rendering

--- Bell/src/Bell/Renderer/Renderer.h
+++ Bell/src/Bell/Renderer/Renderer.h
@@ -1,19 +1,28 @@
 #ifndef _RENDERER_H
+#define _RENDERER_H
 
 #include "RenderCommand.h"
+#include "Shader.h"
+#include "OrthographicCamera.h"
 
 namespace Bell
 {
     class Renderer
     {
     public:
-        static void BeginScene();
+        static void BeginScene(OrthographicCamera& camera);
         static void EndScene();
 
-        static void Submit(const std::shared_ptr<VertexArray>& vertexArray);
+        static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
 
         inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
+    private:
+        struct SceneData
+        {
+            glm::mat4 ViewProjectionMatrix;
+        };
 
+        static SceneData* s_SceneData;
     };
 
 }

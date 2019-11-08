6be75d51f1091533500b387610568fb2f89b1efa
Riley <simplemangame@gmail.com>
2019-11-07 17:43:53 -0800

    Updated names in Renderer2D to reflect changes made to the shader

--- Bell/src/Bell/Renderer/Renderer2D.cpp
+++ Bell/src/Bell/Renderer/Renderer2D.cpp
@@ -18,7 +18,7 @@ namespace Bell
     {
         // TODO: Should be scopes once we control memory
         Ref<VertexArray> QuadVertexArray;
-        Ref<Shader> TextureShader;
+        Ref<Shader> DefaultShader;
         Ref<Texture2D> WhiteTexture;
     };
 
@@ -52,9 +52,9 @@ namespace Bell
         uint32_t whiteTextureData = 0xffffffff;
         s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
 
-        s_Data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
-        s_Data->TextureShader->Bind();
-        s_Data->TextureShader->SetInt("u_Texture", 0);
+        s_Data->DefaultShader = Shader::Create("assets/shaders/Texture.glsl");
+        s_Data->DefaultShader->Bind();
+        s_Data->DefaultShader->SetInt("u_Texture", 0);
     }
 
     void Renderer2D::Shutdown()
@@ -64,8 +64,8 @@ namespace Bell
 
     void Renderer2D::BeginScene(const OrthographicCamera& camera)
     {
-        s_Data->TextureShader->Bind();
-        s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
+        s_Data->DefaultShader->Bind();
+        s_Data->DefaultShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
     }
 
     void Renderer2D::EndScene()
@@ -90,14 +90,14 @@ namespace Bell
     void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, const glm::vec4& color, float textureScale)
     {
         texture->Bind();
-        s_Data->TextureShader->SetFloat4("u_Color", color);
-        s_Data->TextureShader->SetFloat("u_TextureScale", textureScale);
+        s_Data->DefaultShader->SetFloat4("u_Color", color);
+        s_Data->DefaultShader->SetFloat("u_TextureScale", textureScale);
 
         glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
             * glm::rotate(glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f))
             * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-        s_Data->TextureShader->SetMat4("u_Transform", transform);
+        s_Data->DefaultShader->SetMat4("u_Transform", transform);
 
         s_Data->QuadVertexArray->Bind();
         RenderCommand::DrawIndexed(s_Data->QuadVertexArray);

1a1b8fd6783418a937e1871a1d0c4ee762a0b26d
Riley <simplemangame@gmail.com>
2019-11-22 00:50:31 -0800

    Added mouse button enum

--- Bell/src/Bell/Core/Input/MouseButtonCodes.h
+++ Bell/src/Bell/Core/Input/MouseButtonCodes.h
@@ -1,19 +1,41 @@
 #ifndef _MOUSEBUTTONCODES_H
 #define _MOUSEBUTTONCODES_H
 
+namespace Bell
+{
+    enum class MouseButtons
+    {
+        // From glfw3.h
+        Button1 = 0,
+        Button2 = 1,
+        Button3 = 2,
+        Button4 = 3,
+        Button5 = 4,
+        Button6 = 5,
+        Button7 = 6,
+        Button8 = 7,
+
+        ButtonLeft = Button1,
+        ButtonRight = Button2,
+        ButtonMiddle = Button3,
+
+        ButtonLast = Button8,
+    };
+}
+
 // From glfw3.h
 
-#define B_MOUSE_BUTTON_1         0
-#define B_MOUSE_BUTTON_2         1
-#define B_MOUSE_BUTTON_3         2
-#define B_MOUSE_BUTTON_4         3
-#define B_MOUSE_BUTTON_5         4
-#define B_MOUSE_BUTTON_6         5
-#define B_MOUSE_BUTTON_7         6
-#define B_MOUSE_BUTTON_8         7
-#define B_MOUSE_BUTTON_LAST      B_MOUSE_BUTTON_8
-#define B_MOUSE_BUTTON_LEFT      B_MOUSE_BUTTON_1
-#define B_MOUSE_BUTTON_RIGHT     B_MOUSE_BUTTON_2
-#define B_MOUSE_BUTTON_MIDDLE    B_MOUSE_BUTTON_3
+#define B_MOUSE_BUTTON_1      0
+#define B_MOUSE_BUTTON_2      1
+#define B_MOUSE_BUTTON_3      2
+#define B_MOUSE_BUTTON_4      3
+#define B_MOUSE_BUTTON_5      4
+#define B_MOUSE_BUTTON_6      5
+#define B_MOUSE_BUTTON_7      6
+#define B_MOUSE_BUTTON_8      7
+#define B_MOUSE_BUTTON_LAST   B_MOUSE_BUTTON_8
+#define B_MOUSE_BUTTON_LEFT   B_MOUSE_BUTTON_1
+#define B_MOUSE_BUTTON_RIGHT  B_MOUSE_BUTTON_2
+#define B_MOUSE_BUTTON_MIDDLE B_MOUSE_BUTTON_3
 
 #endif // !_MOUSEBUTTONCODES_H

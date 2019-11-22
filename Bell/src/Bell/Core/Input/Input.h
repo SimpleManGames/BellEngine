480799b6d651f815f63b1101fc12f29934f29dcb
Riley <simplemangame@gmail.com>
2019-11-22 01:09:18 -0800

    Added input remapping

--- Bell/src/Bell/Core/Input/Input.h
+++ Bell/src/Bell/Core/Input/Input.h
@@ -6,9 +6,23 @@
 #define _INPUT_H
 
 #include "Bell/Core/Core.h"
+#include "Bell/Core/Input/KeyCodes.h"
+#include "Bell/Core/Input/MouseButtonCodes.h"
+
+#include <variant>
 
 namespace Bell
 {
+    struct KeyAlternative
+    {
+        // Type-safe union
+        std::variant<Keys, MouseButtons> value;
+
+        KeyAlternative() = default;
+        KeyAlternative(Keys key) { value = key; }
+        KeyAlternative(MouseButtons button) { value = button; }
+    };
+
     class Input
     {
     public:
@@ -16,12 +30,44 @@ namespace Bell
         Input(const Input&) = delete;
         Input& operator=(const Input&) = delete;
 
+        // Raw keycode check
+        // Doesn't allow for remapping of keys
         inline static bool IsKeyPressed(int keycode) { return  s_Instance->IsKeyPressedImpl(keycode); }
-        
+        // Raw mouse button check
+        // Doesn't allow for remapping of mouse buttons
         inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
+
         inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
         inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
         inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
+        
+        /// Input remapping functions
+        // Checks to see if the key/button with identifier of name is pressed
+        inline static bool IsInputPressed(const std::string& name)
+        {
+            // Checks if that pair with 'name' exists
+            if (s_Instance->m_Mapping.count(name) != 0)
+            {
+                return IsInputPressed(s_Instance->m_Mapping[name]);
+            }
+
+            B_CORE_WARN("The input {0} is not mapped!", name);
+            return false;
+        }
+        inline static bool IsInputPressed(KeyAlternative input)
+        {
+            if (std::holds_alternative<Keys>(input.value))
+                return s_Instance->IsKeyPressedImpl((int)std::get<Keys>(input.value));
+
+            if (std::holds_alternative<MouseButtons>(input.value))
+                return s_Instance->IsKeyPressedImpl((int)std::get<MouseButtons>(input.value));
+
+            B_CORE_WARN("Unknown KeyAlternative Input");
+            return false;
+        }
+        inline static bool IsMouseButtonPressed(MouseButtons button) { return s_Instance->IsMouseButtonPressedImpl((int)button); }
+
+        inline static void Remap(const std::string& name, KeyAlternative input) { s_Instance->m_Mapping[name] = input; }
 
     protected:
         // Protected so only inherited classes can create an Input
@@ -36,6 +82,9 @@ namespace Bell
         virtual float GetMouseYImpl() = 0;
 
     private:
+        // Used for input remapping
+        std::unordered_map<std::string, KeyAlternative> m_Mapping;
+
         static Scope<Input> s_Instance;
     };
 }

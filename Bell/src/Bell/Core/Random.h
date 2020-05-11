637347eb053c8e7db1e601904b96715778e1c5e6
Riley Smith <riley.smith5345@gmail.com>
2020-05-11 00:03:22 -0700

    Random functions

---
+++ Bell/src/Bell/Core/Random.h
@@ -0,0 +1,24 @@
+#ifndef BELL_RANDOM_H
+#define BELL_RANDOM_H
+
+#include <random>
+
+class Random
+{
+public:
+	static void Init()
+	{
+		s_RandomEngine.seed(std::random_device()());
+	}
+
+	static float Float()
+	{
+		return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
+	}
+
+private:
+	static std::mt19937 s_RandomEngine;
+	static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
+};
+
+#endif // !_RANDOM_H

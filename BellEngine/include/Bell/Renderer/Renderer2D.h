28825a633a23feb7c90226021b6280ca60fe59b3
Riley Smith <riley.smith5345@gmail.com>
2021-01-30 00:21:28 -0800

    Added support to use a transform in the draw quad

--- BellEngine/include/Bell/Renderer/Renderer2D.h
+++ BellEngine/include/Bell/Renderer/Renderer2D.h
@@ -17,14 +17,17 @@ namespace Bell
         static void Init();
         static void Shutdown();
 
-        static void BeginScene(const OrthographicCamera& camera);
+        static void BeginScene(const OrthographicCamera &camera);
         static void EndScene();
         static void Flush();
 
         // Primitives
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotationInRadian, const glm::vec4& color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotationInRadian, const Ref<SubTexture2D>& subTexture, const glm::vec4& colorTint = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), float textureScale = 1.0f);
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float rotationInRadian, const Ref<Texture2D>& texture, const glm::vec4& colorTint = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), float textureScale = 1.0f, const glm::vec2* texCoords = Renderer2D::DefaultTexCoords);
+        static void DrawQuad(const glm::vec3 &position, const glm::vec2 &size, float rotationInRadian, const glm::vec4 &color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+        static void DrawQuad(const glm::vec3 &position, const glm::vec2 &size, float rotationInRadian, const Ref<SubTexture2D> &subTexture, const glm::vec4 &colorTint = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), float textureScale = 1.0f);
+        static void DrawQuad(const glm::vec3 &position, const glm::vec2 &size, float rotationInRadian, const Ref<Texture2D> &texture, const glm::vec4 &colorTint = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), float textureScale = 1.0f, const glm::vec2 *texCoords = Renderer2D::DefaultTexCoords);
+
+        static void DrawQuad(const glm::mat4 &transform, const glm::vec4 &color);
+        static void DrawQuad(const glm::mat4 &transform, const Ref<Texture2D> &texture, const glm::vec4 &colorTint = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), float textureScale = 1.0f, const glm::vec2 *texCoords = Renderer2D::DefaultTexCoords);
 
         // Render stats for debugging
         struct Statistics
@@ -70,6 +73,6 @@ namespace Bell
         /// Resets current batch in order to flush
         static void ResetBatchValues();
     };
-}
+} // namespace Bell
 
 #endif // !_RENDERER2D_H

02df9bd93511172ad3e6944ce1c92de07f984db8
Riley Smith <riley.smith5345@gmail.com>
2021-01-30 20:08:54 -0800

    Doxygen comments for random and time

--- BellEngine/include/Bell/Base/Random.h
+++ BellEngine/include/Bell/Base/Random.h
@@ -6,19 +6,28 @@
 class Random
 {
 public:
-	static void Init()
-	{
-		s_RandomEngine.seed(std::random_device()());
-	}
+    /**
+     * @brief Initializes the random engine with a random seed
+     * 
+     */
+    static void Init()
+    {
+        s_RandomEngine.seed(std::random_device()());
+    }
 
-	static float Float()
-	{
-		return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
-	}
+    /**
+     * @brief Gets a random float
+     * 
+     * @return float Randomized float
+     */
+    static float Float()
+    {
+        return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
+    }
 
 private:
-	static std::mt19937 s_RandomEngine;
-	static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
+    static std::mt19937 s_RandomEngine;
+    static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
 };
 
 #endif // !_RANDOM_H
--- BellEngine/include/Bell/Base/Time.h
+++ BellEngine/include/Bell/Base/Time.h
@@ -6,24 +6,59 @@
 
 #include <chrono>
 
-namespace Bell {
+namespace Bell
+{
 
     class Timestep
     {
     public:
+        /**
+         * @brief Construct a new Timestep object
+         * 
+         */
+        Timestep()
+            : m_Time(0.0f) {}
+        /**
+         * @brief Construct a new Timestep object
+         * 
+         * @param time Initilazes this Timestep with a value already
+         * - Defaults to 0.0f
+         */
         Timestep(float time = 0.0f)
-            : m_Time(time)
-        {
+            : m_Time(time) {}
 
-        }
+        ~Timestep() = default;
+        /**
+         * @brief Copy Constructor for Timestep
+         * 
+         * @param copy Timestep object to copy
+         */
+        Timestep(const Timestep &copy)
+            : m_Time(copy) {}
 
+        /**
+         * @brief Float operator to allow for easy casting and modification
+         * 
+         * @return float Current Time stored within
+         */
         operator float() const { return m_Time; }
 
+        /**
+         * @brief Gets the Time in Seconds
+         * 
+         * @return float Time in Seconds
+         */
         float GetSeconds() const { return m_Time; }
+        /**
+         * @brief Get the Time in Milliseconds
+         * 
+         * @return float Time in Milliseconds
+         */
         float GetMilliseconds() const { return m_Time * 1000.0f; }
+
     private:
         float m_Time;
     };
-}
+} // namespace Bell
 
 #endif // !_TIME_H

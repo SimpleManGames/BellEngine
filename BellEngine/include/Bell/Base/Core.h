b0ae7f3c8da8ff9dde58cd8ffacc57b66c4d1c46
Riley Smith <riley.smith5345@gmail.com>
2021-01-30 22:23:35 -0800

    Missed parentheses

--- BellEngine/include/Bell/Base/Core.h
+++ BellEngine/include/Bell/Base/Core.h
@@ -79,7 +79,7 @@
 
 #define BIT(x) (1 << x)
 
-#define B_BIND_EVENT_FN(fn) [this](auto &&...args) -> decltype(auto) { return this->fn(std::forward<decltype(args)> args...); }
+#define B_BIND_EVENT_FN(fn) [this](auto &&...args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Bell
 {

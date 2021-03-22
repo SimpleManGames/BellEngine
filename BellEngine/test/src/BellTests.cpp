c95a58b1de6ca5d4a665107f7244c4107592e72a
Riley Smith <riley.smith5345@gmail.com>
2021-03-21 21:00:24 -0700

    App Creation test

--- BellEngine/test/src/BellTests.cpp
+++ BellEngine/test/src/BellTests.cpp
@@ -2,7 +2,28 @@
 
 #include <Bell.h>
 
+TEST(BellTests, BellLogInit)
+{
+    Bell::Log::Init();
+    ASSERT_TRUE(Bell::Log::GetClientLogger() != nullptr);
+    ASSERT_TRUE(Bell::Log::GetCoreLogger() != nullptr);
+}
+
 TEST(BellTests, ApplicationCreation)
 {
-    Bell::Application app({});
+    Bell::Log::Init();
+
+    Bell::WindowProps props;
+    props.Title = "Test App";
+    props.Width = 1080;
+    props.Height = 640;
+    Bell::Application *app = new Bell::Application(props);
+
+    Bell::ApplicationState appState = app->GetApplicationState();
+    ASSERT_EQ(appState, Bell::ApplicationState::Initializing);
+
+    // Close
+    app->Close();
+    // Delete the app here, normally EntryPoint.h would do this
+    delete app;
 }
